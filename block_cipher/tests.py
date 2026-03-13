ce9d542753642c46f793f8126ef16bddd2dab8a0
Bryan-13-TY <151882673+Bryan-13-TY@users.noreply.github.com>
2026-03-12 20:05:27 -0600

    Nada

--- block_cipher/tests.py
+++ block_cipher/tests.py
@@ -4,7 +4,7 @@ BASE_DIR = Path(__file__).parent
 
 def files_generator() -> None:
     with open(BASE_DIR / "test.txt", "w") as f:
-        f.write("A" * 200000)
+        f.write("AJD" * 200000)
 
 def main() -> None:
     files_generator()
