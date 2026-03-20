10cb93b8d6b42b6398a2bc11f51a8bc8397c8363
Bryan-13-TY <151882673+Bryan-13-TY@users.noreply.github.com>
2026-03-19 19:54:50 -0600

    Create bit_permutation.c

---
+++ bit_permutation/bit_permutation.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static void print_binary(unsigned char c) {
+    for (int i = 7; i >= 0; i--) {
+        printf("%d", (c >> i) & 1);
+    }
+}
+
+void exercise_1(unsigned char value) {
+    printf("El caracter %c como caracter es: %c\n", value, value);
+    printf("El caracter %c como hexadecimal es: %02X\n", value, value);
+    printf("El caracter %c como entero es: %d", value, value);
+}
+
+void exercise_2(unsigned char value) {
+    for (int i = 0; i < 8; i++) {
+        unsigned char shifted = value << i;
+        printf("Corrimiento %d de %c a la izquierda en caracter: %c, hexadecimal: %02x y entero: %d\n", i + 1, value, shifted, shifted, shifted);
+    }
+}
+
+void exercise_3(unsigned char value) {
+    // A
+    for (int i = 0; i < 8; i++) {
+        unsigned char shifted = value >> i;
+        printf("Corrimiento %d de %c a la derecha en caracter: %c, hexadecimal: %02x y entero: %d\n", i + 1, value, shifted, shifted, shifted);
+    }
+}
+
+void exercise_4(unsigned char v1, unsigned char v2) {
+    printf("%c & %c en char: %c, hexadecimal: %02X, int: %d\n", v1, v2, v1 & v2, v1 & v2, v1 & v2);
+    printf("%c & %c en char: %c, hexadecimal: %02X, int: %d\n", v2, v1, v2 & v1, v2 & v1, v2 & v1);
+    printf("%c | %c en char: %c, hexadecimal: %02X, int: %d\n", v1, v2, v1 | v2, v1 | v2, v1 | v2);
+    printf("%c | %c en char: %c, hexadecimal: %02X, int: %d\n", v2, v1, v2 | v1, v2 | v1, v2 | v1);
+    printf("%c ^ %c en char: %c, hexadecimal: %02X, int: %d\n", v1, v2, v1 ^ v2, v1 ^ v2, v1 ^ v2);
+    printf("%c ^ %c en char: %c, hexadecimal: %02X, int: %d", v2, v1, v2 ^ v1, v2 ^ v1, v2 ^ v1);
+}
+
+void exercise_5(unsigned char data2, unsigned char mask) {
+    printf("El bit mas significativo de %c es: %u", data2, (data2 & mask) >> 7);    
+}
+
+void exercise_6(unsigned char data2, unsigned char mask) {
+    unsigned char result = data2 & mask;
+
+    printf("Los cuatro bits menos significativos de %c son: ", data2);
+
+    for (int i = 3; i >= 0; i--) {
+        printf("%d", (result >> i) & 1);
+    }
+}
+
+int main(int argc, char const *argv[]) {
+    //exercise_6('^', 0x0F);
+    print_binary('^');
+    return 0;
+}
