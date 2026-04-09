80ad9fc34b4838783cd04267dd7e61c199610d67
Bryan-13-TY <151882673+Bryan-13-TY@users.noreply.github.com>
2026-04-08 23:32:10 -0600

    Menú principal terminado

--- main.py
+++ main.py
@@ -1,4 +1,48 @@
-h = 98383784637647324732
+from permutation_cipher import permutation_cipher_menu
+from block_cipher import block_cipher_menu
+from affin_cipher import affin_cipher_menu
+from hill_cipher import hill_cipher_menu
+from shift_cipher import shift_cipher_menu
 
-for i in range(10, 0):
-    print(f"{(h >> i) & 1}", end="")
+from utils import (
+    clean_console,
+    wait_key,
+)
+
+def main() -> None:
+    while True:
+        clean_console()
+        print("""
+/*----------------------------------------------------.
+| INTRODUCTION TO CRYPTOGRAPHY: Algoritmos de cifrado |              
+`----------------------------------------------------*/
+              
+>> Elija una de las opciones
+              
+1.- Cifrar usando 'Shift Cipher'
+2.- Cifrar usando 'Affin Cipher'
+3.- Cifrar usando 'Hill Cipher'
+4.- Cifrar usando 'Permutation Cipher'
+5.- Cifrar usando 'DES'
+6.- Salir
+""")
+        option = input("Opción: ")
+        match option:
+            case "1":
+                shift_cipher_menu()
+            case "2":
+                affin_cipher_menu()
+            case "3":
+                hill_cipher_menu()
+            case "4":
+                permutation_cipher_menu()
+            case "5":
+                block_cipher_menu()
+            case "6":
+                break
+            case _:
+                print(">> Opción no válida")
+                wait_key()
+
+if __name__ == "__main__":
+    main()
