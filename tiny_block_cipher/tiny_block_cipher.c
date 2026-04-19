1a0f4493c7fa87632eb6be71c1c26b4b92bbc84f
Bryan-13-TY <151882673+Bryan-13-TY@users.noreply.github.com>
2026-04-19 13:28:05 -0600

    Corrección de tipo y cosas innecesarias
    Se corrigieron errores en algunas funciones, se eliminaron cosas innecesarias de algunas funciones para esta práctica

--- tiny_block_cipher/tiny_block_cipher.c
+++ tiny_block_cipher/tiny_block_cipher.c
@@ -11,16 +11,11 @@
 
 typedef enum {
     BLOCKC_OK = 0,
-    BLOCKC_SBOX_OUT_OF_RANGE = -1,
-    BLOCKC_SBOX_OPEN_FILE_ERROR = -2,
-    BLOCKC_SBOX_MEMORY_ERROR = -3,
+    BLOCKC_SBOX_OPEN_FILE_ERROR = -1,
+    BLOCKC_SBOX_MEMORY_ERROR = -2,
+    BLOCKC_KEY_OPEN_FILE_ERROR = -3,
+    BLOCKC_KEY_READ_ERROR = -4,
 } BlockCStatus;
-
-typedef struct {
-    unsigned short K0;
-    unsigned short K1;
-    unsigned short K2;
-} SubKeys;
 
 static void wait_key();
 static void clean_buffer();
@@ -28,26 +23,27 @@ static void fill_array(int size, unsigned char array[size]);
 static void shuffle_array(int size, unsigned char array[size]);
 static void generate_textfile_name(char name[], int max_size, int sbox_size);
 static BlockCStatus store_permutation(int size, unsigned char array[size], char textfile_name[]);
-BlockCStatus sbox_generator(int n);
+BlockCStatus sbox_generator();
 BlockCStatus secret_key_generator();
-
-
+static void read_string(int size, char string[size]);
+static BlockCStatus load_sbox(char sbox_filename[], unsigned char sbox[]);
+static BlockCStatus load_key(char key_filename[], unsigned short *K);
+static unsigned short format_block(char block[]);
 static unsigned char get_8MSB(unsigned short K);
 static unsigned char get_8LSB(unsigned short K);
 static unsigned short shuffle_bytes(unsigned char high_byte, unsigned char low_byte);
 static unsigned char get_4MSB(unsigned char c);
 static unsigned char get_4LSB(unsigned char c);
-static unsigned char shuffle_nibbles(unsigned char high_nibble, unsigned char low_nibble);
-static int hex_to_int(char c);
-static BlockCStatus load_sbox(char textfile_name[], unsigned char sbox[], int bits);
-BlockCStatus key_expansion(unsigned short K, SubKeys *sub_keys);
-static void read_string(int size, char string[size]);
+static unsigned char swap_nibbles(unsigned char byte);
+static void key_expansion(unsigned short K, unsigned char sbox[], unsigned short sub_keys[]);
+BlockCStatus encrypt(char sbox_filename[], char key_filename[], char block[], unsigned short sub_keys[]);
 
 int main(int argc, char const *argv[]) {
-    int option = 0, n = 0, temp = 0;
-    unsigned short K, sbox_filename;
+    int option = 0;
+    char key_filename[100], sbox_filename[100], block[10];
+    unsigned short sub_keys[3];
     BlockCStatus blockc_status;
-    SubKeys sub_keys;
+
     srand(time(NULL));
 
     do {
@@ -56,23 +52,18 @@ int main(int argc, char const *argv[]) {
         printf("| Tiny Block Cipher |\n");
         printf("`------------------*/\n\n");
         printf(">> Elije una de las opciones:\n\n");
-        printf("1.- Generar una S-Box\n");
+        printf("1.- Generar una S-Box de 8 bits\n");
         printf("2.- Generar una clave secreta\n");
-        printf("3.- Generar subclaves\n");
-        printf("4.- Cifrar un plaintext\n");
-        printf("5.- Salir de programa\n\n");
+        printf("3.- Cifrar un plaintext\n");
+        printf("4.- Salir del programa\n\n");
         printf("Opcion: ");
         scanf("%d", &option);
         clean_buffer();
 
         switch (option) {
-            case 1:
-                printf("\n>> Escribe el valor de n tal que l = 2^n: ");
-                scanf("%d", &n);
-                
-                blockc_status = sbox_generator(n);
+            case 1:    
+                blockc_status = sbox_generator();
                 if (BLOCKC_OK == blockc_status) printf("\n>>> La S-Box se guardo correctamente");
-                if (BLOCKC_SBOX_OUT_OF_RANGE == blockc_status) printf("\n>>> Debe ser 2 o 3");
                 if (BLOCKC_SBOX_MEMORY_ERROR == blockc_status) printf("\n>>> Hubo un error al reservar memoria para la S-Box");
                 if (BLOCKC_SBOX_OPEN_FILE_ERROR == blockc_status) printf("\n>>> Hubo un error al abrir el archivo de la S-Box");
                 wait_key();
@@ -80,28 +71,27 @@ int main(int argc, char const *argv[]) {
             case 2:
                 blockc_status = secret_key_generator();
                 if (BLOCKC_OK == blockc_status) printf("\n>>> La llave secreta se guardo correctamente");
-                if (BLOCKC_SBOX_OPEN_FILE_ERROR == blockc_status) printf("\n>>> Hubo un error al guardar la llave");
+                if (BLOCKC_KEY_OPEN_FILE_ERROR == blockc_status) printf("\n>>> Hubo un error al guardar la llave");
                 wait_key();
                 break;
             case 3:
-                printf("\n>> Escribe un numero (0 a 65535): ");
-                scanf("%d", &temp);
-                clean_buffer();
-
-                if (temp < 0 || temp > 65535) {
-                    printf("\n>>> El numero no es valido");
+                printf("\n>> Escribe el nombre del archivo con la llave: ");
+                read_string(sizeof(key_filename), key_filename);
+                printf(">> Escribe el nombre del archivo con la S-Box: ");
+                read_string(sizeof(sbox_filename), sbox_filename);
+                printf(">> Escribe el bloque a cifrar (2 caracteres): ");
+                read_string(sizeof(block), block);
+                if (strlen(block) == 2) {
+                    blockc_status = encrypt(sbox_filename, key_filename, block, sub_keys);
+                    if (BLOCKC_SBOX_OPEN_FILE_ERROR == blockc_status) printf("\n>>> Hubo un error al cargar la S-Box");
+                    if (BLOCKC_KEY_OPEN_FILE_ERROR == blockc_status) printf("\n>>> Hubo un error al cargar la llave");
+                    if (BLOCKC_KEY_READ_ERROR == blockc_status) printf("\n>>> Hubo un erro al leer la llave");
                 } else {
-                    K = (unsigned short)temp;
-                    blockc_status = key_expansion(K, &sub_keys);
-                    if (BLOCKC_OK != blockc_status) printf("\n>>> Hubo un error al cargar la S-Box de 8 bits");
-                } 
-
+                    printf("\n>> El tamano del bloque no es correcto");
+                }
                 wait_key();
                 break;
             case 4:
-                printf("\n>> Escribe el plaintext: ");
-                break;
-            case 5:
                 printf("\n>> Gracias por probar el programa");
                 break;
             default:
@@ -109,7 +99,7 @@ int main(int argc, char const *argv[]) {
                 wait_key();
                 break;
         }
-    } while (option != 5);
+    } while(option != 4);
     return 0;
 }
 
@@ -171,8 +161,8 @@ static void generate_textfile_name(char name[], int max_size, int sbox_size) {
  * @param textfile_name Nombre del archivo.
  * 
  * @return
- * - SBOX_OK si se guardo correctamente.
- * - SBOX_OPEN_FILE_ERROR si hubo un error al abrir el archivo.
+ * - BLOCKC_SBOX_OK si se guardo correctamente.
+ * - BLOCKC_SBOX_OPEN_FILE_ERROR si hubo un error al abrir el archivo.
  */
 static BlockCStatus store_permutation(int size, unsigned char array[size], char textfile_name[]) {
     FILE *fp = fopen(textfile_name, "w");
@@ -192,14 +182,12 @@ static BlockCStatus store_permutation(int size, unsigned char array[size], char 
  * @param n Tal que el tamaño de la S-Box es `l` = 2^`n` bits.
  * 
  * @return
- * - SBOX_OK si la S-Box se genero correctamente.
- * - SBOX_OPEN_FILE_ERROR si hubo un error al guardar la S-Box.
- * - SBOX_MEMORY_ERROR si hubo un error al reservar memoria para la S-Box.
+ * - BLOCKC_SBOX_OK si la S-Box se genero correctamente.
+ * - BLOCKC_SBOX_OPEN_FILE_ERROR si hubo un error al guardar la S-Box.
+ * - BLOCKC_SBOX_MEMORY_ERROR si hubo un error al reservar memoria para la S-Box.
  */
-BlockCStatus sbox_generator(int n) {
-    if (n < 2 || n > 3) return BLOCKC_SBOX_OUT_OF_RANGE;
-
-    int l = 1 << n;
+BlockCStatus sbox_generator() {
+    int l = 1 << 2;
     int size_permutation = 1 << l;
 
     unsigned char *array = malloc(size_permutation * sizeof(*array));
@@ -222,17 +210,98 @@ BlockCStatus sbox_generator(int n) {
  * 
  * @return
  * - BLOCKC_OK si la llave se genero correctamente.
- * - BLOCKC_SBOX_OPEN_FILE_ERROR si hubo un error al guardar la llave.
+ * - BLOCKC_KEY_OPEN_FILE_ERROR si hubo un error al guardar la llave.
  */
 BlockCStatus secret_key_generator() {
-    unsigned short K = rand() % 65535;
+    unsigned short K = rand() % 65536;
 
     FILE *fp = fopen("key.txt", "w");
-    if (!fp) return BLOCKC_SBOX_OPEN_FILE_ERROR;
+    if (!fp) return BLOCKC_KEY_OPEN_FILE_ERROR;
 
-    fprintf(fp, "%02X", K);
+    fprintf(fp, "%04X\n", K);
     fclose(fp);
     return BLOCKC_OK;
+}
+
+/**
+ * @brief Lee una cadena de caracteres desde el teclado.
+ * 
+ * @param size Tamaño del arreglo de la cadena.
+ * @param string Cadena leida.
+ */
+static void read_string(int size, char string[size]) {
+    if (fgets(string, size, stdin)) {
+        string[strcspn(string, "\n")] = '\0';
+    } else {
+        string[0] = '\0';
+    }
+}
+
+/**
+ * @brief Carga la S-Box desde un archivo de texto.
+ * 
+ * @param sbox_filename Nombre del archivo.
+ * @param sbox Arreglo en donde se carga la S-Box.
+ * 
+ * @return
+ * - BLOCKC_OK si se cargo correctamente.
+ * - BLOCKC_SBOX_OPEN_FILE_ERROR si hubo un error al abrir el archivo.
+ */
+static BlockCStatus load_sbox(char sbox_filename[], unsigned char sbox[]) {
+    FILE *fp = fopen(sbox_filename, "r");
+    if (!fp) return BLOCKC_SBOX_OPEN_FILE_ERROR;
+
+    // Inicializae la S-Box
+    int size = 1 << 8;
+    for (int i = 0; i < size; i++) {
+        sbox[i] = 0;
+    }
+
+    int input, output;
+    while (fscanf(fp, " %X -> %X", &input, &output) == 2) {
+        if (input >= 0 && input < 256 && output >= 0 && output < 256) {
+            sbox[input] = (unsigned char)output;
+        }
+    }
+
+    fclose(fp);
+    return BLOCKC_OK;
+}
+
+/**
+ * @brief Carga la llave desde un archivo de texto.
+ * 
+ * @param key_filename Nombre del archivo.
+ * @param K Puntero donde se guarda la llave.
+ * 
+ * @return
+ * - BLOCKC_OK si se cargo correctamente.
+ * - BLOCKC_KEY_OPEN_FILE_ERROR si hubo un error al abrir al archivo.
+ * - BLOCKC_KEY_READ_ERROR si hubo un error al leer la llave.
+ */
+static BlockCStatus load_key(char key_filename[], unsigned short *K) {
+    FILE *fp = fopen(key_filename, "r");
+    if (!fp) return BLOCKC_KEY_OPEN_FILE_ERROR;
+
+    if (fscanf(fp, "%X", K) != 1) {
+        fclose(fp);
+        return BLOCKC_KEY_READ_ERROR;
+    }
+
+    fclose(fp);
+
+    return BLOCKC_OK;
+}
+
+/**
+ * @brief Convierte una cadena de 2 caracteres a un bloque de 2 bytes.
+ * 
+ * @param block Cadena.
+ * 
+ * @return Cadena como bloque de 2 bytes.
+ */
+static unsigned short format_block(char block[]) {
+    return ((unsigned char)block[0] << 8) | (unsigned char)block[1];
 }
 
 /**
@@ -272,116 +341,49 @@ static unsigned short shuffle_bytes(unsigned char high_byte, unsigned char low_b
 /**
  * @brief Obtiene el nibble alto (4 bits más significativos) de un byte.
  * 
- * @param c Un byte.
+ * @param byte Un byte.
  * 
  * @return Nibble alto del byte.
  */
-static unsigned char get_4MSB(unsigned char c) {
-    return (c >> 4) & 0x0F;
+static unsigned char get_4MSB(unsigned char byte) {
+    return (byte >> 4) & 0x0F;
 }
 
 /**
  * @brief Obtiene el nibble bajo (4 bits menos significativos) de un byte.
  * 
- * @param c Un byte.
+ * @param byte Un byte.
  * 
  * @return Nibble bajo del byte.
  */
-static unsigned char get_4LSB(unsigned char c) {
-    return c & 0x0F;
+static unsigned char get_4LSB(unsigned char byte) {
+    return byte & 0x0F;
 }
 
 /**
- * @brief Combina el nibble alto y bajo en un byte.
+ * @brief Intercambia el nibble alto y el bajo de un byte.
  * 
- * @param high_nibble Nibble alto de un byte.
- * @param low_nibble Nibble bajo de un byte.
+ * @param byte Un byte.
  * 
- * @return Un Byte.
+ * @return Byte con los nibble intercambiados.
  */
-static unsigned char shuffle_nibbles(unsigned char high_nibble, unsigned char low_nibble) {
-    return (high_nibble << 4) | low_nibble;
-}
-
 static unsigned char swap_nibbles(unsigned char byte) {
     return (get_4LSB(byte) << 4) | get_4MSB(byte);
 }
 
-static int hex_to_int(char c) {
-    if (c >= '0' && c <= '9') {
-        return c - '0';
-    } else if (c >= 'A' && c <= 'F') {
-        return c - 'A' + 10;
-    } else if (c >= 'a' && c <= 'f') {
-        return c - 'a' + 10;
-    } else {
-        return -1;
-    }
-}
-
 /**
- * @brief Carga la S-Box desde un archivo de texto.
+ * @brief Expande la llave secreta en sub-llaves.
  * 
- * @param textfile_name Nombre del archivo.
- * @param sbox Arreglo en donde se carga la S-Box.
- * @param bits Tamaño de la S-Box.
- * 
- * @return
- * - SBOX_OK si se cargo correctamente.
- * - SBOX_OPEN_FILE_ERROR si hubo un error al abrir el archivo.
+ * @param K LLave original.
+ * @param sbox S-Box de 8 bits.
+ * @param sub_keys Arreglo donde se guardan las sub-llaves.
  */
-static BlockCStatus load_sbox(char textfile_name[], unsigned char sbox[], int bits) {
-    FILE *fp = fopen(textfile_name, "r");
-    if (!fp) return BLOCKC_SBOX_OPEN_FILE_ERROR;
-
-    int input, output;
-
-    // Inicializar s-box
-    int size = 1 << bits;
-    for (int i = 0; i < size; i++) {
-        sbox[i] = 0;
-    }
-
-    if (bits == 4) {    
-        char in, out;
-
-        while (fscanf(fp, " %c -> %c", &in, &out) == 2) {
-            input = hex_to_int(in);
-            output = hex_to_int(out);
-
-            if (input != -1 && output != -1) {
-                if (input >= 0 && input < 15 && output >= 0 && output < 16) {
-                    sbox[input] = (unsigned char)output;
-                }
-            }
-        }
-    } else if (bits == 8) {
-        // Evitamos escribir fuera del arreglo
-        while (fscanf(fp, " %X -> %X", &input, &output) == 2) {
-            if (input >= 0 && input < 256 && output >= 0 && output < 256) {
-                sbox[input] = (unsigned char)output;
-            }
-        }
-    } else {
-        fclose(fp);
-        return BLOCKC_SBOX_OUT_OF_RANGE;
-    }
-
-    fclose(fp);
-    return BLOCKC_OK;
-}
-
-BlockCStatus key_expansion(unsigned short K, SubKeys *sub_keys) {
-    unsigned char sbox_8[256];
-    
-    BlockCStatus blockc_status = load_sbox("sbox_8bits.txt", sbox_8, 8);
-    if (BLOCKC_OK != blockc_status) return blockc_status;
-    
+static void key_expansion(unsigned short K, unsigned char sbox[], unsigned short sub_keys[]) {
     unsigned char w0 = get_8MSB(K);
     unsigned char w1 = get_8LSB(K);
-    unsigned char w2 = w0 ^ 0x80 ^ sbox_8[swap_nibbles(w1)];
+    unsigned char w2 = w0 ^ 0x80 ^ sbox[swap_nibbles(w1)];
     unsigned char w3 = w2 ^ w1;
-    unsigned char w4 = w2 ^ 0x30 ^ sbox_8[swap_nibbles(w3)];
+    unsigned char w4 = w2 ^ 0x30 ^ sbox[swap_nibbles(w3)];
     unsigned char w5 = w4 ^ w3;
 
     printf("\n>> Los valores son: \n\n");
@@ -390,26 +392,61 @@ BlockCStatus key_expansion(unsigned short K, SubKeys *sub_keys) {
     printf("w2: %02X\n", w2);
     printf("w3: %02X\n", w3);
     printf("w4: %02X\n", w4);
-    printf("w5: %02X", w5);
+    printf("w5: %02X\n", w5);
 
-    sub_keys->K0 = shuffle_bytes(w0, w1);
-    sub_keys->K1 = shuffle_bytes(w2, w3);
-    sub_keys->K2 = shuffle_bytes(w4, w5);
-
-    return BLOCKC_OK;
+    // Guardamos la sub-llaves
+    sub_keys[0] = shuffle_bytes(w0, w1);
+    sub_keys[1] = shuffle_bytes(w2, w3);
+    sub_keys[2] = shuffle_bytes(w4, w5);
 }
 
 /**
- * @brief Lee una cadena de caracteres desde el teclado.
+ * @brief Descibra un bloque de 2 bytes.
  * 
- * @param size Tamaño del arreglo de la cadena.
- * @param string Cadena leida.
+ * @param sbox_filename Archivo con la S-Box.
+ * @param key_filename Archivo con la llave secreta.
+ * @param block Bloque a cifrar.
+ * @param sub_keys Arreglo con las sub-llaves.
+ * 
+ * @return
+ * - BLOCKC_OK si el cifrado se hizo correctamente.
+ * - BLOCKC_SBOX_OPEN_FILE_ERROR si hubo un error al cargar la S-Box.
+ * - BLOCKC_KEY_OPEN_FILE_ERROR si hubo un error al cargar la llave.
+ * - BLOCKC_KEY_READ_ERROR si hubo un error al leer la llave.
  */
-static void read_string(int size, char string[size]) {
-    if (fgets(string, size, stdin)) {
-        string[strcspn(string, "\n")] = '\0';
-    } else {
-        string[0] = '\0';
-    }
-}
+BlockCStatus encrypt(char sbox_filename[], char key_filename[], char block[], unsigned short sub_keys[]) {
+    unsigned short K;
+    unsigned char sbox[256], L, R;
+    unsigned short M = format_block(block);
+    unsigned short C;
+    BlockCStatus blockc_status;
 
+    // Cargar S-Box y la llave
+    blockc_status = load_sbox(sbox_filename, sbox);
+    if (BLOCKC_OK != blockc_status) return blockc_status;
+
+    blockc_status = load_key(key_filename, &K);
+    if (BLOCKC_OK != blockc_status) return blockc_status;
+
+    // Expandir la llave
+    key_expansion(K, sbox, sub_keys);
+
+    // Cifrado
+    for (int i = 0; i < 3; i ++) {
+        printf("\nIteracion %d:\n", i + 1);
+        printf("\nM <- %04X ^ %04X", M, sub_keys[i]);
+        M = M ^ sub_keys[i];
+        printf(": %04X", M);
+
+        L = get_8MSB(M); R = get_8LSB(M);
+        L = sbox[L]; R = sbox[R];
+
+        printf("\nM <- S(%04X)", M);
+        M = shuffle_bytes(L, R);
+        printf(": %04X\n", M);
+    }
+
+    printf("\n>> El bloque cifrado es: %04X", M);
+
+    return BLOCKC_OK;
+}
