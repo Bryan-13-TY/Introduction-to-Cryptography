f204cb98735b752130377cbec7b21479a03c3cff
Bryan-13-TY <151882673+Bryan-13-TY@users.noreply.github.com>
2026-05-08 23:15:12 -0600

    Multiplicación

---
+++ practicas en C/mult_on_GF/include/mult.h
@@ -0,0 +1,18 @@
+#ifndef MULT_H
+#define MULT_H
+
+unsigned short mult(
+    unsigned short a,
+    unsigned short b,
+    unsigned short r,
+    int n
+);
+
+unsigned short xtime(unsigned short f);
+
+unsigned short fg_mult(
+    unsigned short f,
+    unsigned short g
+);
+
+#endif /* MULT_H */
