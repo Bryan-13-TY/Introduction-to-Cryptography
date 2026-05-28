bbacc273c1aabb2dda9ca6a52b3fabdd5c7bdf2f
Bryan-13-TY <151882673+Bryan-13-TY@users.noreply.github.com>
2026-05-28 13:02:12 -0600

    Modificar algunas cosas

--- practicas en C/tbc_modo_ctr_co/src/ctr_status.c
+++ practicas en C/tbc_modo_ctr_co/src/ctr_status.c
@@ -4,7 +4,7 @@
 void show_possible_error(CTRStatus ctr_status) {
     if (CTR_SBOX_OPEN_FILE_ERROR == ctr_status) {
         printf("\n>>> Hubo un error al cargar la S-Box");
-    } 
+    }
     if (CTR_SBOX_GENERATION_ERROR == ctr_status) {
         printf("\n>>> Hubo un error al generar y guardar la S-Box");
     } 
@@ -33,7 +33,7 @@ void show_possible_error(CTRStatus ctr_status) {
         printf("\n>>> Hay valores repetidos en la P-Box");
     } 
     if (CTR_CIPHER_OPEN_FILE_ERROR == ctr_status) {
-        printf("\n>>> Hubo un error al cargar el texto cifrado");
+        printf("\n>>> Hubo un error al cargar el ciphertext");
     } 
     if (CTR_CIPHER_GENERATION_ERROR == ctr_status) {
         printf("\n>>> Hubo un error al generar y guardar el texto cifrado");
@@ -46,5 +46,17 @@ void show_possible_error(CTRStatus ctr_status) {
     } 
     if (CTR_NUM_BLOCKS_READ_ERROR == ctr_status) {
         printf("\n>>> Hubo un error al leer el numero de bloques del ciphertext");
-    } 
+    }
+    if (CTR_PLAINT_GET_SIZE_FILE_ERROR == ctr_status) {
+        printf("\n>>> Hubo un error al obtener el tamano del archivo con el plaintext");
+    }
+    if (CTR_PLAINT_SIZE_FILE_ERROR == ctr_status) {
+        printf("\n>>> Hubo un error con el tamano del archivo con el plaintext, debe pesar minimo 1 KB");
+    }
+    if (CTR_PLAINT_MEMORY_ERROR == ctr_status) {
+        printf("\n>>> Hubo un error al reservar memoria para el plaintext");
+    }
+    if (CTR_PLAINT_OPEN_FILE_ERROR == ctr_status) {
+        printf("\n>>> Hubo un error al cargar el plaintext");
+    }
 }
